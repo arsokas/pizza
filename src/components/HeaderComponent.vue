a5df40db59d6c628d9916899d0cb99e90c7f3f97
arsokas <arnas.sokolovas@gmail.com>
2020-08-05 20:25:09 +0200

    navbar update

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-carousel height="1200" cycle hide-delimiter-background :show-arrows="false">
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+      <v-row class="fill-height flex-column" align="center" justify="center">
+        <div class="header-text">Example Text</div>
+        <p class="header-info-text">Another Information and Text text text</p>
+      </v-row>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      items: [
+        {
+          src: "https://wallpaperaccess.com/full/424487.jpg"
+        },
+        {
+          src:
+            "https://hdwallpaperim.com/wp-content/uploads/2017/09/17/60012-food-pizza-cheese.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header-text {
+  font-size: 78px;
+  font-weight: bold;
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: black;
+}
+.header-info-text {
+  width: 400px;
+  text-align: center;
+  font-size: 28px;
+}
+</style>
--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,20 +1,50 @@
 <template>
-  <v-app-bar app color="white">
+  <v-toolbar height="100">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-divider class="mx-4" vertical></v-divider>
     <div class="d-flex align-center">
       <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://i.imgur.com/mc0waJE.png"
+        alt="Pizza Logo"
+        src="https://image-cdn.neatoshop.com/styleimg/86189/none/white/default/415564-20;1555584537u.jpg"
         width="100"
       />
     </div>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text class="disable-events">Aalborgade 10, Aalborg</v-btn>
+    </v-toolbar-items>
     <v-spacer></v-spacer>
-  </v-app-bar>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text>Menu</v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>About Us</v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>Reservations</v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text class="disable-events">
+        <v-icon>fas fa-phone</v-icon>
+        <span>45 45 45 45</span>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
+
+
 <script>
 export default {
-  name: "NavBarComponent",
+  name: "NavbarComponent"
 };
 </script>
+
+<style scoped>
+.disable-events {
+  pointer-events: none;
+}
+</style>
+
+
