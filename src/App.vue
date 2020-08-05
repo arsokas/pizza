88804ddb5fc94517926a7952d5d275aeae1de2e3
arsokas <arnas.sokolovas@gmail.com>
2020-08-05 18:58:55 +0200

    first update

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <NavBar />
+      <Home />
+    </v-main>
+    <FooterComponent />
+  </v-app>
+</template>
+
+<script>
+import NavBar from "./components/NavBarComponent";
+import FooterComponent from "./components/FooterComponent";
+import Home from "./components/Home";
+export default {
+  name: "App",
+  data: () => ({}),
+
+  components: {
+    FooterComponent,
+    NavBar,
+    Home,
+  },
+};
+</script>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app color="white">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Name"
+        class="shrink mt-1 hidden-sm-and-down"
+        contain
+        min-width="100"
+        src="https://i.imgur.com/mc0waJE.png"
+        width="100"
+      />
+    </div>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "NavBarComponent",
+};
+</script>
