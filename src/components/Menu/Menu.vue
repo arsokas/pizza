9618c2afe213649fbca1bde2acfcd35c90fbb051
arsokas <arnas.sokolovas@gmail.com>
2020-08-14 14:41:15 +0200

    update on the menu components

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -2,15 +2,27 @@
   <v-container fluid class="pa-0">
     <MenuHeader />
     <MenuMainComponent />
+    <MenuSliderComponent />
+    <MenuSliderComponent2 />
+    <MenuSliderComponent3 />
   </v-container>
 </template>
 
 <script>
 import MenuHeader from "./MenuHeader";
 import MenuMainComponent from "./MenuMainComponent";
+import MenuSliderComponent from "./MenuSliderComponent";
+import MenuSliderComponent2 from "./MenuSliderComponent2";
+import MenuSliderComponent3 from "./MenuSliderComponent3";
 export default {
   name: "Menu",
-  components: { MenuHeader, MenuMainComponent }
+  components: {
+    MenuHeader,
+    MenuMainComponent,
+    MenuSliderComponent,
+    MenuSliderComponent2,
+    MenuSliderComponent3
+  }
 };
 </script>
 
---
+++ src/components/Menu/MenuSliderComponent.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-container fill-height class="my-10">
+    <v-row align="center" justify="center" class="pa-0 image-with-background bordered-container">
+      <v-col class="pa-0" align="center" justify="center">
+        <v-img class="menu-image" v-bind:src="firstImage"></v-img>
+      </v-col>
+      <v-col class="menu-data-table" align="center" justify="center">
+        <v-simple-table class="table-width">
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-center">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                class="menu-item"
+                v-for="item in pizzas"
+                :key="item.name"
+                v-on:click="renderMenuImage(item)"
+              >
+                <td>{{ item.name }}</td>
+                <td class="text-center">{{ item.price }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-chip class="ma-2" color="green" text-color="white">{{ firstItemDescription }}</v-chip>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MenuSliderComponent",
+  data() {
+    return {
+      firstItemDescription:
+        "Click on menu and select pizza to see ingredients and options.",
+      firstImage:
+        "https://vignettepizzeria.com/wp-content/uploads/pizza-2978377_1920-1.jpg",
+      pizzas: [
+        {
+          name: "Margherita",
+          price: 159 + " DKK",
+          src:
+            "https://www.recipetineats.com/wp-content/uploads/2014/06/Pasta1.jpg",
+          ingredients: "Cheese and other delicious ingredients"
+        },
+        {
+          name: "Margherita1",
+          price: 159 + " DKK",
+          src:
+            "https://www.fifteenspatulas.com/wp-content/uploads/2012/11/Tiramisu-Fifteen-Spatulas-1-640x424.jpg",
+          ingredients: "Cheese and other delicious ingredients and more"
+        },
+        {
+          name: "Margherita2",
+          price: 159 + " DKK",
+          src:
+            "https://vignettepizzeria.com/wp-content/uploads/pizza-2978377_1920-1.jpg",
+          ingredients: "Cheese and other delicious ingredients even more"
+        },
+        {
+          name: "Margherita3",
+          price: 159 + " DKK",
+          src:
+            "https://vignettepizzeria.com/wp-content/uploads/pizza-2978377_1920-1.jpg",
+          ingredients: "Cheese and other delicious ingredients"
+        },
+        {
+          name: "Margherita4",
+          price: 159 + " DKK",
+          src:
+            "https://vignettepizzeria.com/wp-content/uploads/pizza-2978377_1920-1.jpg",
+          ingredients: "Cheese and other delicious ingredients"
+        }
+      ]
+    };
+  },
+  methods: {
+    renderMenuImage(item) {
+      console.log(item);
+      this.firstImage = item.src;
+      this.firstItemDescription = item.ingredients;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bordered-container {
+  border: 1px solid black;
+}
+.menu-image {
+  height: 350px;
+  width: 610px;
+}
+
+.table-width {
+  width: 650px;
+}
+
+.menu-item:hover {
+  cursor: pointer;
+}
+
+.image-with-background {
+  background-color: lightslategrey;
+}
+
+.menu-data-table {
+  height: 100%;
+}
+</style>
